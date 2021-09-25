23f6cbaa3824ef3b3e5b7e33f5114cb9c62c622e
Muhammad-07 <muhammad.begawala@gmail.com>
2021-09-25 11:26:03 +0530

    first commit

---
+++ database/seeders/BankAccountSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BankAccount;
+use Illuminate\Database\Seeder;
+
+class BankAccountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $bank_codes =
+            "ICAB
+        HDCB
+        KMZP
+        SBIT
+        IDFC";
+
+        $bank_names =
+            "ICICI BANK LTD
+        HDFC BANK LTD
+        KOTAK MAHINDRA
+        STATE BANK OF INDIA
+        IDFC FIRST";
+
+        $account_numbers =
+            "325682568
+        3658998565
+        986552365
+        357899853
+        300659876";
+
+        $opening_balances =
+            "325685
+        9856855
+        332212
+        124875
+        13386654";
+
+        $bank_codes = preg_split('/\r\n|\r|\n/', $bank_codes);
+        $bank_names = preg_split('/\r\n|\r|\n/', $bank_names);
+        $account_numbers = preg_split('/\r\n|\r|\n/', $account_numbers);
+        $opening_balances = preg_split('/\r\n|\r|\n/', $opening_balances);
+
+        foreach ($bank_codes as $bank_codes_key => $bank_code) {
+            BankAccount::create([
+                'code' => $bank_code,
+                'name' => $bank_names[$bank_codes_key],
+                'ac_no' => $account_numbers[$bank_codes_key],
+                'opening_balance' => (float)$opening_balances[$bank_codes_key]
+            ]);
+        }
+    }
+}
